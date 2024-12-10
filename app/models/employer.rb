bd48fb4ee65de8a9aa4903a8ed458c82634c38a5
MichelleZ96 <michelle.zhang@ChicagoBooth.edu>
2024-12-10 21:02:00 +0000

    added employer table and adjusted sign-in/out settings

---
+++ app/models/employer.rb
@@ -0,0 +1,29 @@
+# == Schema Information
+#
+# Table name: employers
+#
+#  id                       :bigint           not null, primary key
+#  company_description      :text
+#  email                    :string           default(""), not null
+#  encrypted_password       :string           default(""), not null
+#  for_profit_or_non_profit :string
+#  industry                 :string
+#  job_postings_count       :integer
+#  name                     :string
+#  remember_created_at      :datetime
+#  reset_password_sent_at   :datetime
+#  reset_password_token     :string
+#  created_at               :datetime         not null
+#  updated_at               :datetime         not null
+#
+# Indexes
+#
+#  index_employers_on_email                 (email) UNIQUE
+#  index_employers_on_reset_password_token  (reset_password_token) UNIQUE
+#
+class Employer < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
